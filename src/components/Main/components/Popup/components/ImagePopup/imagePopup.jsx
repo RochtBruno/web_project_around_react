665ba3358063a1842cb12a6330482f2293db8602
Bruno Rocha <bruno.taveira.r0.382@gmail.com>
2025-05-12 18:30:46 -0300

    .

--- src/components/Main/components/Popup/components/ImagePopup/imagePopup.jsx
+++ src/components/Main/components/Popup/components/ImagePopup/imagePopup.jsx
@@ -1,26 +1,24 @@
 import closeModal from '../../../../../../images/close-modal.png';
 
 function ImagePopup({ card, onClose }) {
-  if (!card) return null;
+    if (!card) return null;
 
-  return (
-    <div className="popup opened">
-      <div className="popup__content popup__content_content_image">
-        <img
-          className="popup__close"
-          src={closeModal}
-          alt="Fechar"
-          onClick={onClose}
-        />
-        <img
-          className="popup__image"
-          src={card.link}
-          alt={card.name}
-        />
-        <p className="popup__caption">{card.name}</p>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <img
+                className="popup__close"
+                src={closeModal}
+                alt="Fechar"
+                onClick={onClose}
+            />
+            <img
+                className="popup__image"
+                src={card.link}
+                alt={card.name}
+            />
+            <p className="popup__caption">{card.name}</p>
+        </>
+    );
 }
 
 export default ImagePopup;
